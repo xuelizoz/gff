51f3991b349b5bd0755e89f31acb6a79a97ecdfe
xuelizoz <xuelizoz@hotmail.com>
2026-03-21 11:19:10 -0500

    Add sample education credential data

---
+++ src/data/credentials.js
@@ -0,0 +1,16 @@
+const credentials = [
+  {
+    "degree": "Bachelor of Science",
+    "major": "Computer Science",
+    "institution": "University of Technology",
+    "year": 2023
+  },
+  {
+    "degree": "Master of Science",
+    "major": "Software Engineering",
+    "institution": "Institute of Technology",
+    "year": 2025
+  }
+];
+
+module.exports = credentials;
